d2ad6937ef871cd5f7aebf1a948dc5a59fbbae6e
Milan Ševčík <majlen@civ.zcu.cz>
2017-11-02 09:58:21 +0100

    Test initialization of Ruby logging in different way

--- src/test/java/cz/cesnet/cloud/occi/InitTest.java
+++ src/test/java/cz/cesnet/cloud/occi/InitTest.java
@@ -2,10 +2,16 @@ package cz.cesnet.cloud.occi;
 
 import cz.cesnet.cloud.occi.interfaces.Initializer;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class InitTest {
 	@BeforeSuite
 	public void createRubyLogger() {
+		Initializer.initRubyLogger();
+	}
+
+	@Test
+	public void rubyLoggerTest() {
 		Initializer.initRubyLogger();
 	}
 }
