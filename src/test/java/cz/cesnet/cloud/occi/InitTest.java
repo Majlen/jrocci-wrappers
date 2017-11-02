b8add01b50e35b687a61e0f1e97a6bb17a18f1f6
Milan Ševčík <majlen@civ.zcu.cz>
2017-11-02 10:16:46 +0100

    Revert "Test initialization of Ruby logging in different way"
    It's not necessary, correctly configuring jacoco is sufficient.
    
    This reverts commit d2ad6937ef871cd5f7aebf1a948dc5a59fbbae6e.

--- src/test/java/cz/cesnet/cloud/occi/InitTest.java
+++ src/test/java/cz/cesnet/cloud/occi/InitTest.java
@@ -2,16 +2,10 @@ package cz.cesnet.cloud.occi;
 
 import cz.cesnet.cloud.occi.interfaces.Initializer;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 public class InitTest {
 	@BeforeSuite
 	public void createRubyLogger() {
-		Initializer.initRubyLogger();
-	}
-
-	@Test
-	public void rubyLoggerTest() {
 		Initializer.initRubyLogger();
 	}
 }
